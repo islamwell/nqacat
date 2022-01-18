6a2ed5f382c57417c166698482714a88d2775d89
nodeUpdate <tes@mail.com>
2022-01-18 18:12:41 +0500

    latest cron update

--- src/data/config.js
+++ src/data/config.js
@@ -1,2 +1,2 @@
-export const version = "2.998.13";
+export const version = "2.998.14";
 export const domain = "https://add.nurulquran.com"
--- src/service-worker.js
+++ src/service-worker.js
@@ -21,7 +21,7 @@ clientsClaim();
 
 setCacheNameDetails({
     prefix: "nurulquran",
-    suffix: "2.998.26",
+    suffix: "2.998.27",
     precache: "nurulquran-precache",
     runtime: "nurulquran-runtime",
 });
