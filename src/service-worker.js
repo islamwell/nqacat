97b997b90ba80fe079399ddb2c54fc8d6ad7f4eb
nodeUpdate <tes@mail.com>
2022-01-18 09:11:14 +0500

    latest cron update

--- src/service-worker.js
+++ src/service-worker.js
@@ -21,7 +21,7 @@ clientsClaim();
 
 setCacheNameDetails({
     prefix: "nurulquran",
-    suffix: "2.998.7",
+    suffix: "2.998.8",
     precache: "nurulquran-precache",
     runtime: "nurulquran-runtime",
 });
