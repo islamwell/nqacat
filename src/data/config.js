2fb93763b86cba386ffc62d9e4f5af754e39960a
nodeUpdate <tes@mail.com>
2022-02-15 15:28:36 +0500

    latest cron update

--- src/data/config.js
+++ src/data/config.js
@@ -1,2 +1,2 @@
-export const version = "2.998.37";
+export const version = "2.998.38";
 export const domain = "https://nqapp.nurulquran.com"
--- src/service-worker.js
+++ src/service-worker.js
@@ -21,7 +21,7 @@ clientsClaim();
 
 setCacheNameDetails({
     prefix: "nurulquran",
-    suffix: "2.998.50",
+    suffix: "2.998.51",
     precache: "nurulquran-precache",
     runtime: "nurulquran-runtime",
 });
