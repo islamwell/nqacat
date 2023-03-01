cba0e67eaca34649bef56ae11739ab334a33c288
islamwell <live@nrq.no>
2023-03-01 16:40:14 +0500

    Update service-worker.js
    version 3.00

--- src/service-worker.js
+++ src/service-worker.js
@@ -21,7 +21,7 @@ clientsClaim();
 
 setCacheNameDetails({
     prefix: "nurulquran",
-    suffix: "2.998.56",
+    suffix: "3.00",
     precache: "nurulquran-precache",
     runtime: "nurulquran-runtime",
 });
